ab77026fb35d3d04692c32c3b26b5238139eb6ee
Randomaizer <altair@xaker.ru>
2022-07-25 14:02:47 +0300

    Create webhooks.py
    first release

---
+++ webhooks.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,json
+import requests
+from datetime import datetime
+import time
+
+server = "http://127.0.0.1:8096";
+api_key='XXXXXXXXX';
+
+app = Flask(__name__)
+
+@app.route('/jellyfin', methods=['POST'])
+def index():
+   event_data = json.loads(request.get_data())
+   user = event_data['NotificationUsername']
+   device = event_data['DeviceId']
+   action = event_data['NotificationType']
+   x = requests.get(server+"/Sessions?api_key="+api_key+"&deviceId="+device)
+   big_data = x.json()
+   session = big_data[0]["Id"]
+   print(user+" with device_id: "+device+" start session_id: "+session+" with action: "+action)
+   if (action == "PlaybackStart"):
+                  #Ваша подписка действительна до:
+                  url = server+"/Sessions/"+session+"/Message?&api_key="+api_key
+                  Text="Example of message: "
+                  body = {"Text":Text, "Header":"Warning", "TimeoutMs": 20000}
+                  x = requests.post(url, json = body)
+                  print(x)
+   return ''
+
+if __name__ == '__main__':
+    app.run()
